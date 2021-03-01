329410f175066931fb4bd4b78cbf733cd93738f6
choijw1116 <choijw1116@gmail.com>
2021-03-01 18:56:51 +0900

    all

--- my-app/src/components/SmallPoster.tsx
+++ my-app/src/components/SmallPoster.tsx
@@ -13,7 +13,8 @@ function SmallPoster() {
     <article className='smallPoster'>
       <img></img>
       <div className='smallPoster__info'>
-        <span className='smallPoster__name'></span>
+        <span className='smallPoster__name'>포스터 이릉</span>
+        <span className='smallPoster__rate-num'>평점 5.0</span>
       </div>
     </article>
   );

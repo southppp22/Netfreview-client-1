341b4ccca269a0a633b8627ac461efbd8ae3d4eb
choijw1116 <choijw1116@gmail.com>
2021-03-01 17:34:16 +0900

    [add] signup.tsx

---
+++ my-app/src/components/SignUp.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function SignIn() {
+  return (
+    <section className='login'>
+      <img />
+      <div className='login-warp'>
+        <h3></h3>
+      </div>
+    </section>
+  );
+}
+
+export default SignIn;

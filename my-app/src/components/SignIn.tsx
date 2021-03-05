f0f258a40949ff3dd12c53b50b64fa87ae918644
choijw1116 <choijw1116@gmail.com>
2021-03-05 10:09:21 +0900

    [del]&[modify] delete css files & modify components and pages

--- my-app/src/components/SignIn.tsx
+++ my-app/src/components/SignIn.tsx
@@ -3,63 +3,87 @@ import img from '../img/logo.png';
 import facebook from '../img/facebook.png';
 import google from '../img/google.png';
 import kakao from '../img/kakao-talk.png';
-import '../css/SignIn.css';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   Redirect,
-// } from 'react-router-dom';
+import '../scss/SignIn.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import SignUp from './SignUp';
+import Sign from '../pages/Sign';
 // import axios from 'axios';
 type isModalprops = {
   closeModal: () => void;
 };
-const SignIn = ({ closeModal }: isModalprops) => {
+
+function SignIn({ closeModal }: isModalprops) {
+  const [isSignUpOpen, setIsSignUpOpen] = useState<boolean>(false);
+  const [isSignInClose, setIsSignInClose] = useState<boolean>(false);
+
+  const openSignUp = () => {
+    setIsSignUpOpen(true);
+    setIsSignInClose(true);
+  };
+
+  const closeSignUp = () => {
+    setIsSignUpOpen(false);
+  };
+
   return (
-    <div className='signinModal'>
-      <div onClick={closeModal} className='modal-back'></div>
-      <div className='login'>
-        <img className='img' src={img} />
-        <div className='login__wrap'>
-          <h3 className='login-title'>로그인</h3>
-          <form className='login-form'>
-            <input
-              className='login-input'
-              type='text'
-              placeholder='이메일'
-            ></input>
-            <input
-              className='login-input'
-              type='text'
-              placeholder='비밀번호'
-            ></input>
-          </form>
-          <div className='login-btn'>
-            <button id='loginBtn' className='loginButton'>
-              로그인
-            </button>
-            <button id='loginBtn' className='signinButton'>
-              회원가입으로 이동
-            </button>
+    <div>
+      {isSignInClose ? null : (
+        <div className='signinModal'>
+          <div onClick={closeModal} className='modal-back'></div>
+          <div className='login'>
+            <img className='img' src={img} />
+            <div className='login__wrap'>
+              <h3 className='login-title'>로그인</h3>
+              <form className='login-form'>
+                <input
+                  className='login-input'
+                  type='text'
+                  placeholder='이메일'
+                ></input>
+                <input
+                  className='login-input'
+                  type='text'
+                  placeholder='비밀번호'
+                ></input>
+              </form>
+              <div className='login-btn'>
+                <button id='loginBtn' className='loginButton'>
+                  로그인
+                </button>
+                <button
+                  onClick={openSignUp}
+                  id='loginBtn'
+                  className='signinButton'
+                >
+                  회원가입으로 이동
+                </button>
+              </div>
+              <ul className='login-social'>
+                <li className='login-social__wrap'>
+                  <li className='google'>
+                    <img className='logo' src={google} />
+                  </li>
+                  <li className='google'>
+                    <img className='logo' src={kakao} />
+                  </li>
+                  <li className='facebook '>
+                    <img className='logo' src={facebook} />
+                  </li>
+                </li>
+              </ul>
+            </div>
           </div>
-          <ul className='login-social'>
-            <li className='login-social__wrap'>
-              <li className='google'>
-                <img className='logo' src={google} />
-              </li>
-              <li className='google'>
-                <img className='logo' src={kakao} />
-              </li>
-              <li className='facebook '>
-                <img className='logo' src={facebook} />
-              </li>
-            </li>
-          </ul>
         </div>
-      </div>
+      )}
+      {isSignUpOpen ? <SignUp closeModal={closeModal} /> : null}
     </div>
   );
-};
+}
 
 export default SignIn;

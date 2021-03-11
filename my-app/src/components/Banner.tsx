9561a8e3c5af12f44df3df182cdd21dd6957f025
조슬지 <seulji@joseuljiui-iMac.local>
2021-03-12 02:57:05 +0900

    [modify] main and add BannerComponent

---
+++ my-app/src/components/Banner.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { RootState } from '../modules';
+
+type VideoInfo = {
+  id: number;
+  title: string;
+  description: string;
+  bannerUrl: string;
+};
+
+function Banner() {
+  const { top5VideoList } = useSelector(
+    (state: RootState) => state.videoList.videoInfoList
+  );
+
+  const [topVideo, setTopVideo] = useState<VideoInfo>({
+    id: 0,
+    title: '',
+    description: '',
+    bannerUrl: '',
+  });
+
+  useEffect(() => {
+    top5VideoList && setTopVideo(top5VideoList[0]);
+  }, [top5VideoList]);
+
+  return (
+    <section
+      className="banner"
+      style={{
+        backgroundImage: `url(${topVideo?.bannerUrl})`,
+      }}
+    >
+      <div className="bg-color">
+        <div className="banner__container">
+          <article>
+            <h1 className="banner__title">{topVideo?.title}</h1>
+            <p className="banner__synopsis">{topVideo?.description}</p>
+            <Link to={`/review/${topVideo?.id}/?page=1`}>
+              <button>리뷰보러가기</button>
+            </Link>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
--- my-app/src/pages/Main.tsx
+++ my-app/src/pages/Main.tsx
@@ -1,90 +1,47 @@
-import axios from 'axios';
-import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import PosterContainer from '../components/PosterContainer';
-import RecommendedModal from '../components/RecommendedModal';
-
-import '../scss/Main.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../modules';
 import { fetchVideoListThunk } from '../modules/videoList';
-type Video = {
-  id: number;
-  title: string;
-  description: string;
-  director?: string;
-  actor?: string;
-  runtime?: string;
-  ageLimit?: string;
-  releaseYear?: string;
-  posterUrl?: string;
-  bannerUrl: string;
-  netflixUrl?: string;
-  type?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-  rating?: number;
-};
+
+import PosterContainer from '../components/PosterContainer';
+import RecommendedModal from '../components/RecommendedModal';
+import Banner from '../components/Banner';
+
+import '../scss/Main.scss';
+
 function Main() {
   const dispatch = useDispatch();
   const { top5VideoList, mostReviewVidList, lessReviewVidList } = useSelector(
     (state: RootState) => state.videoList.videoInfoList
   );
   const [isModal, setIsModal] = useState<boolean>(false);
-  const [topVideo, setTopVideo] = useState<Video>({
-    id: 0,
-    title: '',
-    description: '',
-    bannerUrl: '',
-  });
 
-  const handleModal = () => {
-    setIsModal(!isModal);
-  };
-  const openModal = () => {
-    setIsModal(true);
-  };
-
-  const closeModal = () => {
-    setIsModal(false);
-  };
+  // const handleModal = () => {
+  //   setIsModal(!isModal);
+  // };
+  // const openModal = () => {
+  //   setIsModal(true);
+  // };
+  // const closeModal = () => {
+  //   setIsModal(false);
+  // };
 
   useEffect(() => {
     dispatch(fetchVideoListThunk('main'));
-  }, []);
-  useEffect(() => {
-    top5VideoList && setTopVideo(top5VideoList[0]);
-  }, [top5VideoList]);
+  }, [dispatch]);
 
   return (
     <main>
-      <button className="btn-recommend" onClick={openModal}>
+      <button className="btn-recommend" onClick={() => setIsModal(true)}>
         <span>
           이거
           <br />
           어때?
         </span>
       </button>
-      <RecommendedModal open={isModal} close={closeModal} />
+      <RecommendedModal open={isModal} close={setIsModal} />
       <div className="bg-color__top"></div>
-      <section
-        className="banner"
-        style={{
-          backgroundImage: `url(${topVideo?.bannerUrl})`,
-        }}
-      >
-        <div className="bg-color">
-          <div className="banner__container">
-            <article>
-              <h1 className="banner__title">{topVideo?.title}</h1>
-              <p className="banner__synopsis">{topVideo?.description}</p>
-              <Link to={`/review/${topVideo?.id}/?page=1`}>
-                <button>리뷰보러가기</button>
-              </Link>
-            </article>
-          </div>
-        </div>
-      </section>
+      <Banner />
       <PosterContainer
         subTitle="넷프리뷰가 추천하는 탑 5"
         videoList={top5VideoList}

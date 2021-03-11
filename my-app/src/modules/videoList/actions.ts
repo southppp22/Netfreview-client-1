73d6fe9ad67c1fe075592cc9fd90b01c027638d0
southppp22 <tnddl032@gmail.com>
2021-03-12 06:07:07 +0900

    [Add]resetVideoList
    비디오 리스트 리셋 시키는 함수 추가

--- my-app/src/modules/videoList/actions.ts
+++ my-app/src/modules/videoList/actions.ts
@@ -1,10 +1,14 @@
 import { AxiosError } from 'axios';
-import { createAsyncAction } from 'typesafe-actions';
+import { createAction, createAsyncAction } from 'typesafe-actions';
 import { VideoList } from '../videoList';
 
 export const FETCH_VIDEOLIST = 'video/FETCH_VIDEOLIST';
 export const FETCH_VIDEOLIST_SUCCESS = 'video/FETCH_VIDEOLIST_SUCCESS';
 export const FETCH_VIDEOLIST_FAILURE = 'video/FETCH_VIDEOLIST_FAILURE';
+
+export const RESET_VIDEOLIST = 'video/RESET_VIDEOLIST';
+
+export const resetVideoList = createAction(RESET_VIDEOLIST)<undefined>();
 
 export const fetchVideoListAsync = createAsyncAction(
   FETCH_VIDEOLIST,

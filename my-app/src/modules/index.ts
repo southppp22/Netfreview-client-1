74d052975fa98f295fcb4b8a70940ae854345c1c
namin kim <68219675+southppp22@users.noreply.github.com>
2021-03-11 23:53:12 +0900

    Update index.ts

--- my-app/src/modules/index.ts
+++ my-app/src/modules/index.ts
@@ -3,6 +3,7 @@ import userInfo from './userInfo';
 import review from './review';
 import isLogin from './isLogin';
 import login from './login';
+import videoList from './videoList';
 import video from './video';
 
 const rootReducer = combineReducers({

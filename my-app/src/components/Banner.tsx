3300ed4efbc801b78866b0924a2d190d8f8dd8dd
조슬지 <seulji@joseuljiui-iMac.local>
2021-03-12 19:07:09 +0900

    [add]Banner

--- my-app/src/components/Banner.tsx
+++ my-app/src/components/Banner.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from '../modules';
+
+import BannerSlide from './BannerSlide';
+import 'swiper/swiper.scss';
 
 type VideoInfo = {
   id: number;
@@ -11,7 +15,7 @@ type VideoInfo = {
 };
 
 function Banner() {
-  const { top5VideoList } = useSelector(
+  const { top5VideoList, lessReviewVidList } = useSelector(
     (state: RootState) => state.mainVideo.videoInfoList
   );
 
@@ -21,30 +25,36 @@ function Banner() {
     description: '',
     bannerUrl: '',
   });
+  const [lessVideo, setLessVideo] = useState<VideoInfo>({
+    id: 0,
+    title: '',
+    description: '',
+    bannerUrl: '',
+  });
 
   useEffect(() => {
     top5VideoList && setTopVideo(top5VideoList[0]);
-  }, [top5VideoList]);
+    lessReviewVidList && setLessVideo(lessReviewVidList[0]);
+  }, [top5VideoList, lessReviewVidList]);
 
   return (
-    <section
-      className="banner"
-      style={{
-        backgroundImage: `url(${topVideo?.bannerUrl})`,
-      }}
+    <Swiper
+      className="swiper-container"
+      spaceBetween={0}
+      slidesPerView={1}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      loop={true}
+      style={{ height: '555px' }}
     >
-      <div className="bg-color">
-        <div className="banner__container">
-          <article>
-            <h1 className="banner__title">{topVideo?.title}</h1>
-            <p className="banner__synopsis">{topVideo?.description}</p>
-            <Link to={`/review/${topVideo?.id}/?page=1`}>
-              <button>리뷰보러가기</button>
-            </Link>
-          </article>
-        </div>
-      </div>
-    </section>
+      <SwiperSlide>
+        <BannerSlide subTitle="넷프리뷰의 NO.1" video={topVideo} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <BannerSlide video={lessVideo} />
+      </SwiperSlide>
+      <SwiperSlide> slide3 </SwiperSlide>
+    </Swiper>
   );
 }
 
---
+++ my-app/src/components/BannerSlide.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function BannerSlide({ subTitle, video }: any) {
+  return (
+    <section
+      className="banner"
+      style={{
+        backgroundImage: `url(${video.bannerUrl})`,
+      }}
+    >
+      <div className="bg-color">
+        <div className="banner__container">
+          <article>
+            <p className="banner__subtitle">{subTitle}</p>
+            <h1 className="banner__title">{video.title}</h1>
+            <p className="banner__synopsis">{video.description}</p>
+            <Link to={`/review/${video.id}/?page=1`}>
+              <button>리뷰보러가기</button>
+            </Link>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default BannerSlide;
